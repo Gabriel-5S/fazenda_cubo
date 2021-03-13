e78e0b3b78922427f58c38e212a22a68a9ed598e
Gabriel-5S <gabriel.r@usp.br>
2021-03-13 04:23:47 -0300

    Muda o local da classe do botão do scanner e adiciona o envio dos dados scaneados para as telas seguintes

---
+++ lib/components/qr_code.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
+
+import '../screens/activity_screen.dart';
+import '../models/local.dart';
+
+class QrcodeButton extends StatefulWidget {
+  @override
+  _QrcodeButtonState createState() => _QrcodeButtonState();
+}
+
+class _QrcodeButtonState extends State<QrcodeButton> {
+  
+  _scanner(BuildContext context) async {
+    String cameraScanResult = await scanner.scan();
+
+    Local local = new Local();
+    
+    local.id = cameraScanResult;
+
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) {
+        return ActivityScreen(local);
+      }),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(
+        Icons.qr_code,
+        color: Colors.white,
+      ),
+      onPressed: () => _scanner(context),
+      backgroundColor: Theme.of(context).primaryColor,
+    );
+  }
+}
